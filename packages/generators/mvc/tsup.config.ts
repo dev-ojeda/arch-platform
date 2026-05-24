405cecb5bb2691bd4b3ceb7a6a129fc906ab4ba1
dev-ojeda <neo1sr3@gmail.com>
2026-05-24 19:26:55 -0400

    refactor(generators): align mvc generator build strategy

--- packages/generators/mvc/tsup.config.ts
+++ packages/generators/mvc/tsup.config.ts
@@ -1,11 +1,20 @@
+// packages\generators\mvc\tsup.config.tsi
 import { defineConfig } from 'tsup';
 
 import { baseConfig } from '../../../tsup.base.ts';
 
 export default defineConfig({
   ...baseConfig,
+
   tsconfig: './tsconfig.build.json',
 
   entry: ['src/index.ts'],
+
+  format: ['esm'],
+
+  bundle: true,
+
+  splitting: false,
+
   dts: false,
 });
