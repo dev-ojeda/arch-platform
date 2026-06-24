2aa073487d505112110429a78c53738b33173869
dev-ojeda <neo1sr3@gmail.com>
2026-06-24 19:05:31 -0400

    feat(code-analysis): add semantic package dependency model

---
+++ packages/code-analysis/src/symbol-dependencies/index.ts
@@ -0,0 +1,5 @@
+// packages/code-analysis/src/symbol-dependencies/index.ts
+
+export * from './symbol-dependency-graph.js';
+export * from './symbol-dependency-types.js';
+export * from './symbol-dependency.js';
