98a1e592bdc8b02bc9e4649d08e8b78906408169
dev-ojeda <neo1sr3@gmail.com>
2026-06-24 19:08:50 -0400

    feat(code-analysis): add exported symbol index

---
+++ packages/code-analysis/src/symbol-dependencies/exported-symbol-index.ts
@@ -0,0 +1,1 @@
+// packages/code-analysis/src/symbol-dependencies/exported-symbol-index.ts
--- packages/code-analysis/src/symbol-dependencies/index.ts
+++ packages/code-analysis/src/symbol-dependencies/index.ts
@@ -1,5 +1,8 @@
 // packages/code-analysis/src/symbol-dependencies/index.ts
 
+export * from './exported-symbol-index-builder.js';
+export * from './exported-symbol-index.js';
+export * from './exported-symbol.js';
 export * from './symbol-dependency-graph.js';
 export * from './symbol-dependency-types.js';
 export * from './symbol-dependency.js';
