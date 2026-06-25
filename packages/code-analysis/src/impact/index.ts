e5efc9905ea91e33e5052a0ca23ca29fff1b01e7
dev-ojeda <neo1sr3@gmail.com>
2026-06-25 16:13:07 -0400

    feat(code-analysis): add impact scoring

--- packages/code-analysis/src/impact/index.ts
+++ packages/code-analysis/src/impact/index.ts
@@ -2,3 +2,5 @@
 
 export { ImpactAnalyzer } from './impact-analyzer.js';
 export type { ImpactResult } from './impact-result.js';
+export { ImpactScoreCalculator } from './impact-score-calculator.js';
+export type { ImpactRisk, ImpactScoreResult } from './impact-score-result.js';
