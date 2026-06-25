c3ded583a2e68f39f243ba01dbfe73109cc25989
dev-ojeda <neo1sr3@gmail.com>
2026-06-25 01:47:41 -0400

    feat(code-analysis): implement semantic dependency analyzer

--- packages/code-analysis/src/symbol-dependencies/index.ts
+++ packages/code-analysis/src/symbol-dependencies/index.ts
@@ -3,3 +3,4 @@
 export * from './package-dependency-graph-builder.js';
 export * from './package-dependency-graph.js';
 export * from './package-dependency.js';
+export * from './semantic-dependency-analyzer.js';
--- packages/code-analysis/src/symbol-dependencies/semantic-dependency-analyzer.ts
+++ packages/code-analysis/src/symbol-dependencies/semantic-dependency-analyzer.ts
@@ -1,1 +1,17 @@
 // packages/code-analysis/src/symbol-dependencies/semantic-dependency-analyzer.ts
+
+import type { Project } from 'ts-morph';
+
+import { buildSymbolGraph } from '../symbol-graph/build-symbol-graph.js';
+
+import type { PackageDependencyGraphBuilder } from './package-dependency-graph-builder.js';
+import type { PackageDependencyGraph } from './package-dependency-graph.js';
+
+export class SemanticDependencyAnalyzer {
+  constructor(private readonly graphBuilder: PackageDependencyGraphBuilder) {}
+  analyze(project: Project): PackageDependencyGraph {
+    const graph = buildSymbolGraph(project);
+
+    return this.graphBuilder.build(graph);
+  }
+}
