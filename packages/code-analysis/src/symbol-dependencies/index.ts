d20b22032e7c46b333edafc58af03d6bfda6b7df
dev-ojeda <neo1sr3@gmail.com>
2026-06-25 01:35:01 -0400

    feat(code-analysis): implement package dependency graph builder

--- packages/code-analysis/src/symbol-dependencies/index.ts
+++ packages/code-analysis/src/symbol-dependencies/index.ts
@@ -1,9 +1,5 @@
 // packages/code-analysis/src/symbol-dependencies/index.ts
 
-export * from './semantic-dependency-analyzer.js';
-
-export * from './symbol-dependency-types.js';
-export * from './symbol-dependency.js';
-
+export * from './package-dependency-graph-builder.js';
 export * from './package-dependency-graph.js';
 export * from './package-dependency.js';
