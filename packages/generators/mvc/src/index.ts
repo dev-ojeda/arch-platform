7e8a03e44e6cf0cdaa62167c31583ac9e95c8419
dev-ojeda <neo1sr3@gmail.com>
2026-06-28 19:22:59 -0400

    feat(generators): update mvc package build output

--- packages/generators/mvc/src/index.ts
+++ packages/generators/mvc/src/index.ts
@@ -1,8 +1,8 @@
 // packages\generators\mvc\src\index.ts
 
 // Probando 1
-export * from './definition/index.js';
-export * from './variables/index.js';
+export { mvcDescriptor, mvcFiles, mvcGenerator, mvcSchema } from './definition/index.js';
+export type { MvcVariables } from './variables/index.js';
 
 export { mvcModule } from './registration/index.js';
 // test 3
